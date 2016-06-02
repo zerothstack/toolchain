cc43a8eda92b96ef6f8d6f3b6de53bd015168dbc
Zak Henry <zak.henry@gmail.com>
2016-06-02 17:51:15 +0100

    feat(deployment): reordered deployment script so paths are correct

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -2,16 +2,16 @@
 
 MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 .travis/deployment_key.pem # this key should have push access
+ssh-add .travis/deployment_key.pem
+
 u doc:build
 u doc:api
 cd dist-docs
 git init
 git add --all
 git commit -am "$MESSAGE"
-
-eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 .travis/deployment_key.pem # this key should have push access
-ssh-add .travis/deployment_key.pem
 
 git remote add deploy git@github.com:ubiquits/ubiquits.github.io.git
 git push deploy master -f
