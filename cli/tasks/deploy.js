08cfaf721c9391b16801de6098cbebe23e717a53
Zak Henry <zak.henry@gmail.com>
2016-06-08 20:42:16 +0100

    feat(cli): Major refactor of the doc deployment strategy.
    Fixes #4

--- cli/tasks/deploy.js
+++ cli/tasks/deploy.js
@@ -33,125 +33,251 @@ function task(cli, project) {
 
 function gitDeploy(project, cli, config) {
 
-  return new Promise((resolve) => {
+  const initBranch = 'deploy';
 
+  return new Promise((resolve, reject) => {
 
-  const dir     = path.resolve(project.basePath, config.dir);
-  const pkg     = require(project.basePath + '/package.json');
-  const repoUrl = pkg.repository.url.split(/\.git$|^git\+/).filter(p=>!!p).pop();
+    const dir     = path.resolve(project.basePath, config.dir);
+    const pkg     = require(project.basePath + '/package.json');
+    const repoUrl = pkg.repository.url.split(/\.git$|^git\+/).filter(p=>!!p).pop();
 
-  let index, baseRepository, repository, author, commit;
+    let index, baseRepository, deployRepository, remote, author, indexId, commit;
+    let noRemoteBranch = false;
 
+    return git.Repository.open(project.basePath)
+      .then((repo) => {
+        baseRepository = repo;
+        cli.log(`finding last commit at ${project.basePath}`);
+        return repo.getHeadCommit();
+      })
+      .then((c) => {
+        commit = c;
 
+        const commitAuthor = commit.author();
 
-  return git.Repository.open(project.basePath)
-    .then((repo) => {
-      baseRepository = repo;
-      cli.log(`finding last commit at ${project.basePath}`);
-      return repo.getHeadCommit();
-    })
-    .then((c) => {
-      commit = c;
-      author = commit.author();
-      cli.log(`Found commit: ${author.name()} - ${commit.message()}`);
-    })
-    .then(() => {
-      cli.log(`Initializing repo at ${dir}`);
-      return git.Repository.init(dir, 0);
-    })
-    .then((repo) => {
-      repository = repo;
-      cli.log(`refreshing index`);
-      return repo.refreshIndex();
-    })
-    .then((idx) => {
-      index = idx;
-      cli.log(`adding files`);
-      return index.addAll();
-    })
-    .then(() => {
+        author = git.Signature.now(commitAuthor.name(), commitAuthor.email());
 
-      const totalFileSize = index.entries().reduce((sum, file) => sum + file.fileSize, 0);
+        cli.log(`Found commit: ${commitAuthor.name()} - ${commit.message()}`);
+      })
+      .then(() => {
+        cli.log(`Initializing repo at ${dir}`);
+        return git.Repository.initExt(dir, {
+          initialHead: initBranch
+        }).then(r => deployRepository = r);
+      })
+      .then(() => getRemoteRepo(cli, config, baseRepository, deployRepository).then(r => remote = r))
+      .then(() => deployRepository.refreshIndex())
+      .then((idx) => {
+        index = idx;
+        cli.log(`adding files`);
+        return index.addAll();
+      })
+      .then(() => {
 
-      cli.log(`Added ${index.entryCount()} files totalling ${filesize(totalFileSize)}`);
+        const totalFileSize = index.entries().reduce((sum, file) => sum + file.fileSize, 0);
 
-      cli.log(`writing tree`);
-      return index.writeTree();
-    })
-    .then((oid) => {
-      cli.log(`committing`);
-      return repository.createCommit("HEAD", author, author, `Docs deploy: | ${commit.message()} - ${repoUrl}/commit/${commit.id()}`, oid, []);
-    })
-    .then(() => {
-      if (!!config.repo) {
-        cli.log(`creating configure repo remote`);
-        return git.Remote.create(repository, config.remote, config.repo);
-      }
+        cli.log(`Added ${index.entryCount()} files totalling ${filesize(totalFileSize)}`);
 
-      cli.log(`retrieving remote url from parent repo`);
-      return git.Remote.lookup(baseRepository, config.remote)
-        .then((remote) => {
-          cli.log(`found remote url ${remote.url()}`);
-
-          if (remote.name() == 'origin' && config.branch == 'master') {
-            throw new Error(`refusing to set remote to root origin and branch to master. You probably want to configure brance gh-pages or a different repo`);
-          }
-
-          return git.Remote.create(repository, remote.name(), remote.url());
-        });
-    })
-    .then((remote) => {
-      cli.log(`added remote: ${remote.name()} ${remote.url()}`);
-      cli.log(`pushing`);
-
-      let certCheckCount = 0;
-
-      return remote.push([`+refs/heads/master:refs/heads/${config.branch}`], {
-        callbacks: {
-          certificateCheck: () => 1,
-          credentials: (url, userName) => {
-            if (certCheckCount >= 1) {
-              throw new Error('Runaway certificate checking detected, aborting')
+        cli.log(`writing tree`);
+        return index.writeTree().then(oid => indexId = oid);
+      })
+      .then(() => {
+        cli.log(`fetching remote ${config.branch}`);
+        return remote.fetch(config.branch, {
+          callbacks: {
+            certificateCheck: () => 1,
+            credentials: (url, userName) => {
+              if (certCheckCount >= 1) {
+                throw new Error('Runaway certificate checking detected, aborting')
+              }
+              cli.log(`getting creds from agent url:${url} username:${userName}`);
+              certCheckCount++;
+              return git.Cred.sshKeyFromAgent(userName);
+            },
+            transferProgress: (progress) => {
+              cli.log('progress: ', progress)
             }
-            cli.log(`getting creds from agent url:${url} username:${userName}`);
-            certCheckCount++;
-            return git.Cred.sshKeyFromAgent(userName);
-          },
-          transferProgress: (progress) => {
-            cli.log('progress: ', progress)
           }
+        })
+          .catch((e) => {
+
+            if (e.message !== 'Callback failed to initialize SSH credentials') {
+              throw e;
+            }
+
+            cli.log('nodegit push failed, falling back to native shell');
+            return new Promise((resolve, reject) => {
+              const cmd = spawn('git', ['fetch', remote.name(), config.branch], {
+                cwd: dir,
+                stdio: [0, 1, 2]
+              });
+
+              cmd.on('close', (code) => {
+                if (code !== 0) {
+                  return reject(new Error(`child process exited with code ${code}`));
+                }
+
+                resolve();
+              });
+
+              cmd.on('error', (err) => {
+                return reject(err);
+              });
+            });
+          })
+          .catch((e) => { //remote doesn't exist, create local branch
+            noRemoteBranch = true;
+          });
+      })
+      .then(() => {
+        if (noRemoteBranch){
+          return commitOnHead(cli, config, deployRepository, commit, author, indexId, repoUrl)
+        } else {
+          return commitOnParent(cli, config, deployRepository, commit, author, indexId, repoUrl);
         }
-      }).catch((e) => {
 
-        cli.log('nodegit push failed, falling back to native shell');
-        return new Promise((resolve, reject) => {
-          const cmd = spawn('git', ['push', remote.name(), `HEAD:${config.branch}`, '-f'], {
-            cwd: dir,
-            stdio: [0,1,2]
-          });
+      })
+      .then((oid) => {
+        return git.Commit.lookup(deployRepository, oid);
+      })
+      .then((commit) => {
+        cli.log(`resetting head`);
+        return git.Reset(deployRepository, commit, git.Reset.TYPE.HARD);
+      })
+      .then(() => {
+        cli.log(`added remote: ${remote.name()} ${remote.url()}`);
+        cli.log(`pushing`);
 
-          cmd.on('close', (code) => {
-            if (code !== 0) {
-              throw new Error(`child process exited with code ${code}`);
+        let certCheckCount = 0;
+
+        return remote.push([`+HEAD:${config.branch}`], {
+          callbacks: {
+            certificateCheck: () => 1,
+            credentials: (url, userName) => {
+              if (certCheckCount >= 1) {
+                throw new Error('Runaway certificate checking detected, aborting')
+              }
+              cli.log(`getting creds from agent url:${url} username:${userName}`);
+              certCheckCount++;
+              return git.Cred.sshKeyFromAgent(userName);
+            },
+            transferProgress: (progress) => {
+              cli.log('progress: ', progress)
             }
+          }
+        }).catch((e) => {
 
-            resolve();
+          cli.log('nodegit push failed, falling back to native shell');
+          return new Promise((resolve, reject) => {
+            const cmd = spawn('git', ['push', remote.name(), `HEAD:${config.branch}`], {
+              cwd: dir,
+              stdio: [0, 1, 2]
+            });
+
+            cmd.on('close', (code) => {
+              if (code !== 0) {
+                throw new Error(`child process exited with code ${code}`);
+              }
+
+              resolve();
+            });
+
+            cmd.on('error', (err) => {
+              throw err;
+            });
           });
 
-          cmd.on('error', (err) => {
-            throw err;
-          });
         });
 
+      })
+      .then(() => {
+        cli.log(`Push complete.`);
+        resolve();
+      }).catch((e) => {
+        switch (e.message) {
+          case 'no_changes':
+            cli.log('No changes, exiting');
+            break;
+          default:
+            throw reject(e);
+        }
       });
-    })
-    .then(() => {
-      cli.log(`Push complete.`);
-      resolve();
-    });
-
 
   });
+
+}
+
+function commitOnHead(cli, config, deployRepository, commit, author, indexId, repoUrl) {
+
+  cli.log(`committing`);
+  return deployRepository.createCommit("HEAD", author, author, `First Docs deploy: | ${commit.message()} - ${repoUrl}/commit/${commit.id()}`, indexId, []);
+
+}
+
+
+function commitOnParent(cli, config, deployRepository, commit, author, indexId, repoUrl) {
+
+  let parentCommit;
+
+  cli.log(`Checking out fetched remote branch ${config.remote}/${config.branch}`);
+  return git.Branch.lookup(deployRepository, `${config.remote}/${config.branch}`, git.Branch.BRANCH.REMOTE)
+    .then((ref) => deployRepository.checkoutRef(ref, {
+      checkoutStrategy: git.Checkout.STRATEGY.ALLOW_CONFLICTS | git.Checkout.STRATEGY.USE_OURS
+    }))
+    .then(() => git.Reference.nameToId(deployRepository, "HEAD"))
+    .then(head => deployRepository.getCommit(head).then(parent => parentCommit = parent))
+    .then(() => {
+      cli.log('Checking status');
+      return deployRepository.getStatus()
+        .then((statuses) => {
+          if (statuses.length === 0) {
+            throw new Error('no_changes');
+          }
+          // @todo resolve why the status is NEW for all wit the allow conflict strategy
+          cli.log(`${statuses.length} changes detected, committing and pushing them`);
+        });
+    })
+    .then(() => {
+      cli.log(`committing`);
+      return deployRepository.createCommit("HEAD", author, author, `Docs deploy: | ${commit.message()} - ${repoUrl}/commit/${commit.id()}`, indexId, [parentCommit]);
+    });
+
+}
+
+function getRemoteRepo(cli, config, baseRepository, deployRepository) {
+
+  if (!!config.repo) {
+    cli.log(`creating configure repo remote`);
+    return git.Remote.create(deployRepository, config.remote, config.repo);
+  }
+
+  cli.log(`retrieving remote url from parent repo`);
+  return git.Remote.lookup(baseRepository, config.remote)
+    .then((remote) => {
+      cli.log(`found remote url ${remote.url()}`);
+
+      if (remote.name() == 'origin' && config.branch == 'master') {
+        throw new Error(`refusing to set remote to root origin and branch to master. You probably want to configure brance gh-pages or a different repo`);
+      }
+
+      if (remote.url().match(/@/)) { //check for @, assume ssl
+        return git.Remote.create(deployRepository, remote.name(), remote.url());
+      }
+
+      const githubMatcher = /http[s]:\/\/github.com\/(.+?.git)/;
+
+      let match = remote.url().match(githubMatcher);
+
+      if (!match) {
+        throw new Error('Non-github https url provided, could not translate to ssl remote. You will need to manually configure the remote setting');
+      }
+
+      let sslUrl = `git@github.com:${match[1]}`;
+
+      cli.log(`HTTPS github remote url detected, translated to ssl url: ${sslUrl}`);
+
+      return git.Remote.create(deployRepository, remote.name(), sslUrl);
+    });
 
 }
 
